333970b479aed12dd9184f9851a0b7e82286d6a6
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-28 18:13:32 +0300

    Tue Jul 28 18:13:32 EEST 2026

--- projects/bfc/render_txt.sh
+++ projects/bfc/render_txt.sh
@@ -8,8 +8,11 @@ dir=$pwd
 mv myEpub.zip myEpub
 cd myEpub
 unzip myEpub.zip
-cd myEpub/EPUB/xhtml
+cd EPUB/xhtml
 for n in $(ls [0-9]*.xhtml); do
   cat $n|grep "^.p.*p.$" |cut -b 4,$-4>>$dir/output.txt
 done
+cd $dir
+rm -rf myEpub
+cat output.txt
 echo "Done."
