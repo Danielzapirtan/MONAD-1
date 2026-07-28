8793d3eaa322457f5815c8e7182945191598f2e6
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-07-28 18:40:45 +0300

    Tue Jul 28 18:40:45 EEST 2026

--- projects/bfc/render_txt.sh
+++ projects/bfc/render_txt.sh
@@ -4,13 +4,16 @@ ARG="$1"
 
 cp "$ARG" myEpub.zip
 mkdir -p myEpub
-dir=$pwd
+dir=$PWD
 mv myEpub.zip myEpub
 cd myEpub
 unzip myEpub.zip
 cd EPUB/xhtml
 for n in $(ls [0-9]*.xhtml); do
-  cat $n | sed -e "s/^.p.//g" | sed -e "s/..p.$//g" >> /tmp/tmp
+	cat $n \
+		| grep "^.p.*p.$" \
+		| sed -e "s/^.p.//g" \
+		| sed -e "s/..p.$//g" >> /tmp/tmp
 done
 cd $dir
 rm -rf myEpub
