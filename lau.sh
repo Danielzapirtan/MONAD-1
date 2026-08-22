5de5368912904887c1feab3f71d5418da6bb4447
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-22 03:00:09 +0300

    sâmbătă 22 august 2026, 03:00:09 +0300

--- lau.sh
+++ lau.sh
@@ -2,11 +2,20 @@
 
 APPS="$(ls projects)"
 
+brew install python-is-python3
+pkill -kill python
 pkill -kill python3.12
+deactivate &>/dev/null || true
+find . -type d -iname "*venv" | xargs rm -rf
+rm -rf $HOME/.cache/pip
+python -m venv .venv
+export VIRTUAL_ENV
+source .venv/bin/activate
+
 for APP in $APPS; do
-  bash test.sh $APP &>/tmp/monad_$APP.log
+  bash utest.sh $APP || true
   echo "$APP launched"
-done
+done | cat -n
 echo "All apps have been launched"
 echo "See them on ports 5030, 5034 and 5005"
 echo "Launch terminated"
--- test.sh
+++ test.sh
@@ -5,12 +5,6 @@ APP="$1"
 
 cd ./projects/$APP
 
-brew install python@$VER
-if test -z $VIRTUAL_ENV; then
-	test -d venv || python$VER -m venv venv
-	source venv/bin/activate
-	export VIRTUAL_ENV
-fi
 brew install ffmpeg
 pip install -r requirements.txt
 pip install whispermlx
