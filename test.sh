283bf6dd83ac9682163269cb939fb6b609938cfb
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-22 14:40:17 +0300

    sâmbătă 22 august 2026, 14:40:17 +0300

--- test.sh
+++ test.sh
@@ -36,10 +36,8 @@ fi
 
 LOG=/tmp/requirements_$SECRET.log
 if pip install -r requirements.txt &>$LOG; then
-  echo ""
   echo "Successfully installed requirements"
 else
-  echo ""
   echo "Failed to install python requirements"
   tail $LOG
   false
@@ -48,10 +46,8 @@ fi
 LOG=/tmp/whispermlx_$SECRET.log
 if ! $DEMO; then
   if pip install whispermlx &>$LOG; then
-    echo ""
     echo "Successfully installed whispermlx"
   else
-    echo ""
     echo "Failed to install whispermlx"
     tail $LOG
     false
@@ -60,11 +56,9 @@ fi
 
 LOG=/tmp/app_$SECRET.log
 if ! python$VER app.py &>$LOG; then
-  echo ""
   echo "Failed to launch $APP"
   tail $LOG
   false
 else
-  echo ""
   echo "Successfully launch $APP"
 fi &
